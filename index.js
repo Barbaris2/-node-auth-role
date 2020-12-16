8175a81efcdbdf3ed418d648e8720374371218ba
Stolyarov <t9818184233@gmailcom>
2020-12-16 20:55:24 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRouter = require('./authRouter');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use('/auth', authRouter);
+
+const start = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://dbUser:qwerty123@cluster0.kmd11.mongodb.net/auth_roles?retryWrites=true&w=majority`,
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    );
+    app.listen(PORT, () => {
+      console.log(`server started on port: ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
